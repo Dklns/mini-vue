461a72c700af5eeb57e51d05e989e5c6f8638613
Dklns <d.klns.245@gmail.com>
2023-05-15 22:55:48 +0800

    update: reactive, readonly

---
+++ src/reactivity/baseHandler.ts
@@ -0,0 +1,40 @@
+import {track, trigger} from './effect';
+
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+function createGetter(readonly = false) {
+    return function (target,key) {
+        const res = Reflect.get(target, key);
+
+        if(!readonly) {
+        track(target, key);
+        }
+
+        return res;
+    }
+}
+
+function createSetter() {
+    return function(target,key,value) {
+        const res = Reflect.set(target,key,value);
+
+        // 触发依赖
+        trigger(target, key);
+        return res;
+    }
+}
+
+export const mutableHandlers = {
+    get,
+    set,
+}
+
+export const readonlyHandlers = {
+    get: readonlyGet,
+    set: (target, key, value) => {
+        console.warn(`key:${key} set 失败 因为 target readonly`, target);
+        return true
+    }
+}
