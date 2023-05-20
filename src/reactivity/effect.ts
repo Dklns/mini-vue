dcc308df58cea7d89ef2b8279a1f78bef1fe1eac
Dklns <d.klns.245@gmail.com>
2023-05-20 20:43:40 +0800

    update: computed

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,7 +2,7 @@ import { extent } from "./shared";
 let activeEffect;
 let shouldTrack;
 
-class ReactiveEffect {
+export class ReactiveEffect {
     private _fn:any;
     public scheduler:Function | undefined;
     onStop?:() => void;
